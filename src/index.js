34846ad4c52f59d6dd23ba32f2674801d9cbeecb
Hanene Maafi <fh_maafi@esi.dz>
2019-08-07 13:15:17 +0100

    fix: app start

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 // load all env variables from .env file into process.env object.
 require('dotenv').config()
+const env = require("./config/env")
 
 global.__basedir = __dirname;
-const db = require('./config/db.js');
+//const db = require('./config/db.js');
 // force: true will drop the table if it already exists
-db.sequelize.sync({force: true}).then(() => {
+/*db.sequelize.sync({force: true}).then(() => {
     console.log('Drop and Resync with { force: true }');
 })
 
-db.sequelize
+db.sequelize*/
 
 // Require the framework and instantiate it
 const fastify = require('fastify')({
     logger: true
 })
 
-process.on('uncaughtException', function (exception) {
-    console.log(exception); // to see your exception details in the console
-    // if you are on production, maybe you can send the exception details to your
-    // email as well ?
-});
 
 // Import Swagger Options
 const swagger = require("./config/swagger")
@@ -37,21 +33,12 @@ const routes = require("./routes")
 routes.forEach((route, index) => {
     fastify.route(route)
 })
-const passport = require("passport");
-/*const path = require('path')
 
-fastify.register(require('fastify-static'), {
-    root: path.join(__dirname, 'public')
-   // prefix: '/public/', // optional: default '/'
-})*/
-const passportConf = require('./config/passport');
-
-fastify.use(passportConf(passport).initialize())
 // Run the server!
 const start = async () => {
     try {
-        const port = envVarsController.getPort()
-        const address = envVarsController.getHostAddress()
+        const port = env.getPort()
+        const address = env.getHostAddress()
         fastify.listen(port, address,function () {
             fastify.swagger()
             console.log('server listening on ' + port)
