9889f660104664db5105bd8d1b8121890ca98970
Hanene Maafi <fh_maafi@esi.dz>
2019-08-16 23:52:33 +0100

    feat: extract ElMassa latest articles

---
+++ src/web_scrapers/elMassaWebScraper.js
@@ -0,0 +1,84 @@
+const ElMassa = require("./news_sources/El_Massa");
+const axios = require('axios');
+const cheerio = require('cheerio');
+const boom = require('boom')
+
+let getData = (html, category) => {
+    const articles = [];
+    const $ = cheerio.load(html);
+    $('.item-list .item-list-row').each((i, article) =>{
+
+        articles.push({
+            id: i,
+            img:  $(article).find('article .item-image a img').attr('src'),
+            url: $(article).find('article .item-image a').attr('href'),
+            title: $(article).find('article .item-image a').attr('title'),
+            date: $(article).find('article .item-content .item-date time').attr('datetime'),
+            category: category
+        });
+    });
+    return articles;
+}
+
+getPoliticsArticles = async () => {
+    axios.get(ElMassa.categories.POLITICS.url)
+        .then(response => {
+            //console.log(getData(response.data, "POLITICS"))
+            return getData(response.data, "POLITICS");
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+exports.getSportsArticles = async () => {
+    axios.get(ElMassa.categories.SPORTS.url)
+        .then(response => {
+            console.log(getData(response.data, "SPORTS"));
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+exports.getTechArticles = async () => {
+    return []
+}
+
+exports.getCultureArticles = async () => {
+    axios.get(ElMassa.categories.CULTURE.url)
+        .then(response => {
+            return getData(response.data, "CULTURE");
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+exports.getEconomyArticles = async () => {
+    axios.get(ElMassa.categories.ECONOMY.url)
+        .then(response => {
+            return getData(response.data, "ECONOMY");
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+exports.getHealthArticles = async () => {
+    return []
+}
+
+exports.getInternationalArticles = async () => {
+    axios.get(ElMassa.categories.INTERNATIONAL.url)
+        .then(response => {
+            return getData(response.data, "INTERNATIONAL");
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+
+
+
---
+++ src/web_scrapers/news_sources/El_Massa.js
@@ -0,0 +1,35 @@
+module.exports = {
+    url: "https://www.el-massa.com/dz/",
+    logo: "https://www.el-massa.com/dz/images/logo.svg",
+    language: "ar",
+    categories: {// Avaibale categories (comparing to DZ Now v 1.0.0)
+        CULTURE: {
+            isAvailable: true,
+            url: "https://www.el-massa.com/dz/%D8%A7%D9%84%D8%AB%D9%82%D8%A7%D9%81%D8%A9"
+        },
+        HEALTH: {
+            isAvailable: false,
+            url: ""
+        },
+        INTERNATIONAL: {
+            isAvailable: true,
+            url: "https://www.el-massa.com/dz/%D8%A7%D9%84%D8%B9%D8%A7%D9%84%D9%85"
+        },
+        POLITICS: {
+            isAvailable: true,
+            url: "https://www.el-massa.com/dz/%D8%A7%D9%84%D8%A3%D8%AE%D9%8A%D8%B1%D8%A9"
+        },
+        SPORTS: {
+            isAvailable: true,
+            url: "https://www.el-massa.com/dz/%D8%A7%D9%84%D8%B1%D9%8A%D8%A7%D8%B6%D8%A9"
+        },
+        ECONOMY: {
+            isAvailable: false,
+            url: ""
+        },
+        TECH: {
+            isAvailable: false,
+            url: ""
+        },
+    }
+}
