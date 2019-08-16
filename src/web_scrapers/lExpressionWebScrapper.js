ef2f7ed932140831c61d2d2e3942adb4bfd34ec7
Hanene Maafi <fh_maafi@esi.dz>
2019-08-16 23:20:37 +0100

    feat: extract L'Expression latest articles

---
+++ src/web_scrapers/lExpressionWebScrapper.js
@@ -0,0 +1,83 @@
+const Expression = require("./news_sources/Lexpression");
+const axios = require('axios');
+const cheerio = require('cheerio');
+const boom = require('boom')
+
+let getData = (html, category) => {
+    const articles = [];
+    const $ = cheerio.load(html);
+    $('.list-categories li').each( (i, article) =>{
+
+        articles.push({
+            id: i,
+            img:  $(article).find('figure img').attr('src'),
+            url: $(article).find('header h2 a').attr('href'),
+            title: $(article).find('header p').text().split('\n')[5] + '\n' + $(article).find('header h2 a').text(),
+            date: $(article).find('header p span').text().split('\n')[2],
+            category: category
+        });
+    });
+    return articles;
+}
+
+exports.getPoliticsArticles = async () => {
+    axios.get(Expression.categories.POLITICS.url)
+        .then(response => {
+            console.log(getData(response.data, "POLITICS"))
+           // return getData(response.data, "POLITICS");
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+exports.getSportsArticles = async () => {
+    axios.get(Expression.categories.SPORTS.url)
+        .then(response => {
+            console.log(getData(response.data, "SPORTS"));
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+exports.getTechArticles = async () => {
+    return []
+}
+
+exports.getCultureArticles = async () => {
+    axios.get(Expression.categories.CULTURE.url)
+        .then(response => {
+            return getData(response.data, "CULTURE");
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+exports.getEconomyArticles = async () => {
+    axios.get(Expression.categories.ECONOMY.url)
+        .then(response => {
+            return getData(response.data, "ECONOMY");
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+exports.getHealthArticles = async () => {
+    return []
+}
+
+exports.getInternationalArticles = async () => {
+    axios.get(Expression.categories.INTERNATIONAL.url)
+        .then(response => {
+            return getData(response.data, "INTERNATIONAL");
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+
+
---
+++ src/web_scrapers/news_sources/Lexpression.js
@@ -0,0 +1,35 @@
+module.exports = {
+    url: "https://www.lexpressiondz.com/",
+    logo: "",
+    language: "fr",
+    categories: {// Avaibale categories (comparing to DZ Now v 1.0.0)
+        CULTURE: {
+            isAvailable: true,
+            url: "https://www.lexpressiondz.com/culture"
+        },
+        HEALTH: {
+            isAvailable: false,
+            url: ""
+        },
+        INTERNATIONAL: {
+            isAvailable: true,
+            url: "https://www.lexpressiondz.com/internationale"
+        },
+        POLITICS: {
+            isAvailable: true,
+            url: "https://www.lexpressiondz.com/nationale"
+        },
+        SPORTS: {
+            isAvailable: true,
+            url: "https://www.lexpressiondz.com/sports"
+        },
+        ECONOMY: {
+            isAvailable: true,
+            url: "https://www.lexpressiondz.com/economie"
+        },
+        TECH: {
+            isAvailable: false,
+            url: ""
+        },
+    }
+}
